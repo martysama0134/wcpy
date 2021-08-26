5c2d5916811bf771a8972c00fb00e8b1892125c5
Marty Sama <martysama0134@gmail.com>
2021-08-26 11:00:08 +0200

    wcpy draft

---
+++ wcpy/wcpy.h
@@ -0,0 +1,73 @@
+#ifndef __WCPY__WCPY_H__
+#define __WCPY__WCPY_H__
+#pragma once
+
+#include "wcpy_link.h"
+
+namespace wcpy
+{
+class App
+{
+public:
+	App() {
+		Init();
+	}
+	~App() {
+		Clear();
+	}
+
+	static void Init() {
+		Py_Initialize();
+	}
+	static void Clear() {
+		Py_Finalize();
+	}
+
+	static PyObject * GetModule(const char * str) {
+		return PyImport_ImportModule(str);
+	}
+	static PyObject * GetModule(const std::string & str) {
+		return GetModule(str.c_str());
+	}
+
+	/*static PyObject * InitModule(const char * name, PyMethodDef * methods) {
+		return Py_InitModule(name, methods);
+	}*/
+	static int AddObject(PyObject * module, const char * name, PyObject * value) {
+		return PyModule_AddObject(module, name, value);
+	}
+	static int AddObject(const char * module, const char * name, PyObject * value) {
+		return AddObject(GetModule(module), name, value);
+	}
+
+	static int AddString(PyObject * module, const char * name, const char * value) {
+		return PyModule_AddStringConstant(module, name, value);
+	}
+	static int AddString(PyObject * module, const char * name, const std::string value) {
+		return PyModule_AddStringConstant(module, name, value.c_str());
+	}
+	static int AddString(const char * module, const char * name, const char * value) {
+		return AddString(GetModule(module), name, value);
+	}
+	static int AddString(const char * module, const char * name, const std::string & value) {
+		return AddString(GetModule(module), name, value);
+	}
+
+	static int AddInt(PyObject * module, const char * name, long value) {
+		return PyModule_AddIntConstant(module, name, value);
+	}
+	static int AddInt(const char * module, const char * name, long value) {
+		return AddInt(GetModule(module), name, value);
+	}
+
+	static void RunString(const char* str) {
+		PyRun_SimpleString(str);
+	}
+	static void RunString(const std::string & str) {
+		RunString(str.c_str());
+	}
+};
+} // namespace wcpy
+
+
+#endif
---
+++ wcpy/wcpy_link.h
@@ -0,0 +1,8 @@
+#ifndef __WCPY__WCPY_LINK_H__
+#define __WCPY__WCPY_LINK_H__
+#pragma once
+
+// Some projects may have different include paths, edit them here.
+#include <Python.h>
+
+#endif
