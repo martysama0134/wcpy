6141813b661cc6167143cf9ef12fbc37d501d816
Marty Sama <martysama0134@gmail.com>
2021-08-27 09:58:11 +0200

    test code

--- wcpy_test/wcpy_test.cpp
+++ wcpy_test/wcpy_test.cpp
@@ -2,19 +2,82 @@
 //
 
 #include <iostream>
+#include "../wcpy/wcpy.h"
+
+
+static PyObject* CosModule_CosFunc(PyObject* self, PyObject* args)
+{
+    double value;
+    double answer;
+
+    /*  parse the input, from python float to c double */
+    if (!PyArg_ParseTuple(args, "d", &value))
+        return NULL;
+    /* if the above function returns -1, an appropriate Python exception will
+     * have been set, and the function simply returns NULL
+     */
+
+     /* call cos from libm */
+    answer = cos(value);
+
+    /*  construct the output from cos, from c double to python float */
+    return Py_BuildValue("f", answer);
+}
+
+static PyMethodDef CosMethods[] =
+{
+     {"CosFunc", CosModule_CosFunc, METH_VARARGS, "evaluate the cosine"},
+     {NULL, NULL, 0, NULL}
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef cModPyDem =
+{
+    PyModuleDef_HEAD_INIT,
+    "CosModule", "Some documentation",
+    -1,
+    CosMethods
+};
+
+PyMODINIT_FUNC PyInit_CosModule(void)
+{
+    return PyModule_Create(&cModPyDem);
+}
+#endif
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    puts("start"); 
+    {
+        auto app = wcpy::App();
+        app.RunString("from time import time,ctime\n"
+            "print('Today is', ctime(time()))\n");
+        //auto * mod = PyModule_New("CosModule");
+        //PyModule_AddFunctions(mod, CosMethods);
+#if PY_MAJOR_VERSION >= 3
+        PyInit_CosModule();
+        PyRun_SimpleString(
+            "import importlib.abc\n" \
+            "import importlib.machinery\n" \
+            "import sys\n" \
+            "\n" \
+            "\n" \
+            "class Finder(importlib.abc.MetaPathFinder):\n" \
+            "    def find_spec(self, fullname, path, target=None):\n" \
+            "        if fullname in sys.builtin_module_names:\n" \
+            "            return importlib.machinery.ModuleSpec(\n" \
+            "                fullname,\n" \
+            "                importlib.machinery.BuiltinImporter,\n" \
+            "            )\n" \
+            "\n" \
+            "\n" \
+            "sys.meta_path.append(Finder())\n" \
+        );
+#else
+        //PyModule_AddFunctions(mod, CosMethods);
+        Py_InitModule("CosModule", CosMethods);
+#endif
+        app.RunString("import CosModule\nprint(CosModule.cos_func(1.0))");
+    }
+    puts("stop");
 }
-
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
